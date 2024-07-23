6e42c14287760c71d5cf574c40a39685e771a414
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-23 11:12:47 +0530

    add array master

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // .for each 
-const numbers =[1,2,3,4,5];
-numbers.forEach((item,index,arr) => {
-    console.log(arr);
+const numbers =[1,2,3,4,5,10,15,25];
+let sum =0;
+
+
+numbers.forEach(item=> {
+    sum += item;
+   
 });
+console.log(sum);
