2c3f25a349dc6cbf4074115d9c7a480ee9f4b619
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-23 11:42:53 +0530

    add array master

--- arraymap.js
+++ arraymap.js
@@ -1,21 +1,4 @@
-const products = [
-{
-    name:'laptop',
-    price:10000,
-    color:'black',
-    count:5
-},{
-    name:'desktop',
-    price:20000,
-    count:2
-},{
-    name:'phone',
-    price:5000,
-    count:10
-}
-];
+const str =['1','2','3','4'];
 
-const totalProductValue =products.map(item => item.price *item.count );
-
-
-console.log(totalProductValue);
+const numbers =str.map(Number);
+console.log(numbers);
