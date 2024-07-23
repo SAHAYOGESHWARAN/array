176921ef57da5816283eb871759555e7b2e25735
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-23 14:33:01 +0530

    add array master

--- join.js
+++ join.js
@@ -1,4 +1,7 @@
-const countries = ['india', 'russia', 'korea'];
+const countries = ['india', 'russia', 'korea','uk'];
 
-const res = countries.join('');
-console.log(res);
+const res = countries.join(',');
+console.log('i want to visit'  + res);
+
+//output
+//i want to visit india,russia,korea,uk
