80a069c4b4346a5211339bf3c307418f9d57c03d
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-23 13:29:21 +0530

    add array master

---
+++ concat.js
@@ -0,0 +1,16 @@
+const a =[1,2,3];
+const b = [4,5,6];
+const d =[7,8,9];
+
+const c = a.concat(b,d);
+console.log(c); 
+console.log(a); 
+console.log(b);
+ console.log(d); 
+
+
+ // output 
+ //[1, 2, 3 ]
+ //[ 4, 5, 6 ]
+ //[ 7, 8, 9 ]
+ 
