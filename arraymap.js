9644dc5b32cfb45915d3aafcea4d4ad89627ad77
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-23 13:23:39 +0530

    add array master

--- arraymap.js
+++
@@ -1,4 +0,0 @@
-const str =['1','2','3','4'];
-
-const numbers =str.map(Number);
-console.log(numbers);
--- sort.js
+++ sort.js
@@ -1,9 +1,27 @@
-const names = ['saha','gokul','abi','stepan'];
+const products = [
+    {
+        product: 'laptop',
+        value: 1000,
+      
+    },
+    {
+        product: 'desktop',
+        value: 7000,
+      
+    },
+    {
+        product: 'mobile',
+        value: 17000,
+       
+    },
+    {
+        product: 'air buds',
+        value: 700,
+       
+    },
+];
 
-names.sort(); 
-console.log(names);
-
-
-//output
-
-//[ 'abi', 'gokul', 'saha', 'stepan' ]
+products.sort((a,b) => {
+    return b.price-a .price;
+});
+console.log(products);
