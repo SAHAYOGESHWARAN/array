5be2b241411908886f357992c5d2b15dc233c19e
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-23 14:10:27 +0530

    add array master

---
+++ includes.js
@@ -0,0 +1,12 @@
+const fruits =['apple', 'mango'];
+
+const res = fruits.includes('orange');
+if(res){
+    console.log('yuppy!');
+}
+else{
+    console.log("sad.... let buy a orange ");
+}
+
+//output
+//sad.... let buy a orange 
