782a144b70627fabaace6a73555de86e15ea65b4
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-23 11:05:50 +0530

    add array master

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const numbers =[1,2,3,4,5];
 numbers.forEach(consoleItem)
 
 function consoleItem(item,index,arr) {
-    console.log('a[' + item + ']=' + index);
+    console.log('a[' + item + ']=' + item);
 }
