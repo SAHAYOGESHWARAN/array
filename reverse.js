9a1ae58fdb9838936765ec7cd106f00f31e50dc6
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-24 08:41:33 +0530

    add array master

--- reverse.js
+++ reverse.js
@@ -1,9 +1,9 @@
 const numbers = [1,2,3,4,5];
 
-const newArr = numbers.concat().reverse();
+const newArr =[...numbers].reverse();
 console.log(numbers);
 console.log(newArr);
 
 //output
-// 1, 2, 3, 4, 5 ]
+//[ 1, 2, 3, 4, 5 ]
 //[ 5, 4, 3, 2, 1 ]
