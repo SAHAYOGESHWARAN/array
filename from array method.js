ae07ec32affab2de25b5907aa4b0683f68c82d5e
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-24 10:55:46 +0530

    add array master

--- from array method.js
+++ from array method.js
@@ -2,9 +2,13 @@ const str ='1234567';
 
 //[1,2,3,4,5,6,7]
 
-const res = Array.from(str);
+const res = Array.from(str,mapFn);
+function mapFn(x){
+    return Number(x);
+
+}
 console.log(res);
 
 
 //output
-//['1', '2', '3','4', '5', '6','7' ]
+//[ 1, 2, 3, 4, 5, 6, 7 ]
